0991b65a0798af0a70999bf31d7d7280e9f05d90
Luca Lamanna <luca.lamanna@hotmail.com>
2020-02-20 11:33:21 -0500

    first commit

---
+++ src/test-component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TestComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HELLO ECUADOR!</h1>
+            </div>
+        )
+    }
+}
+
+export default TestComponent;
