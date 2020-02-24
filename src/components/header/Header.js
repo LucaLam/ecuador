9c10b76ccd23a67b292dcd1af8dfedc53decdd7f
Luca Lamanna <luca.lamanna@hotmail.com>
2020-02-24 10:22:23 -0500

    header styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
         <div className='header-container'>
             <img className='logo' src={logo} alt='logo'/>
             <p className='header'>This is a header</p>
+            <p className='header'>About</p>
         </div>
     )
 }
