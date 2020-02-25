f53394674267375b5dcb90dd4a4da245e75e5ee1
Luca Lamanna <luca.lamanna@hotmail.com>
2020-02-25 15:02:01 -0500

    now uses react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 
 import Map from './components/map/Map';
 import Header from './components/header/Header';
+import About from './pages/About';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 
 const App =()=> {
     
     return (
-      <>
+      <Fragment>
+      <BrowserRouter>
       <Header />
-      <Map />
-      </>
+      <Switch>
+      <Route path='/' exact component={Map} />
+      <Route path='/about' component={About} />
+      </Switch>
+      </BrowserRouter>
+      </Fragment>
     );
   }
-  
+
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,16 +3,20 @@ import React from 'react';
 import './header.styles.scss'
 import logo from '../../assets/center-of-the-world.svg'
 
+import {Link} from 'react-router-dom'
+
 const Header = () => {
-    return(
-        <div className='header-container'>
-            <img className='logo' src={logo} alt='logo'/>
-            <p className='header'>All You Need Is Ecuador</p>
-            {/* <Link to='/about'> */}
-            <p className='about'>About</p>
-            {/* </Link> */}
-        </div>
-    )
+    return (
+      <div className="header-container">
+        <Link to="/" className="logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <p className="header">All You Need Is Ecuador</p>
+        <Link to="/about" className="about">
+          About
+        </Link>
+      </div>
+    );
 }
 
 export default Header
